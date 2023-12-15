8e9a0ecddb59cd547087a09c86ca1a33f2e80670
jairo <jairo@JAIRO-PC>
2023-12-14 19:07:13 -0600

    Agregar archivos de proyecto.

---
+++ PM2E3206/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using PM2E3206.Vistas;
+namespace PM2E3206
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(Create), typeof(Create));
+            Routing.RegisterRoute(nameof(PageView), typeof(PageView));
+        }
+    }
+}
